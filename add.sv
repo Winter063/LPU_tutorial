35d3d63e8bad7a1c2da1438f558a795d096e8cc0
Winter063 <1962361419@qq.com>
2026-07-12 21:02:38 +0800

    解耦合2

--- add.sv
+++ add.sv
@@ -17,72 +17,37 @@ module add #(
 
     logic                  pipe_valid;
     logic [P_CH*A_BIT-1:0] pipe_data;
+    logic                  handshake_in;
     logic                  handshake_out;
     logic [P_CH*A_BIT-1:0] calc_result;
 
     assign handshake_in  = in1_valid && in1_ready && in2_valid && in2_ready;
     assign handshake_out = out_valid && out_ready;
+    
 
-
-    logic                  buf1_valid;
-    logic [P_CH*A_BIT-1:0] buf1_data;
-    logic                  buf1_consumed; 
-
-    assign in1_ready = !buf1_valid;
-
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            buf1_valid <= 1'b0;
-            buf1_data  <= '0;
-        end else begin
-            if (in1_valid && in1_ready) begin
-                buf1_valid <= 1'b1;
-                buf1_data  <= in1_data;
-            end else if (buf1_valid && buf1_consumed) begin
-                buf1_valid <= 1'b0;
-            end
-        end
-    end
-
-    logic                  buf2_valid;
-    logic [P_CH*A_BIT-1:0] buf2_data;
-    logic                  buf2_consumed;
-
-    assign in2_ready = !buf2_valid;
-
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            buf2_valid <= 1'b0;
-            buf2_data  <= '0;
-        end else begin
-            if (in2_valid && in2_ready) begin
-                buf2_valid <= 1'b1;
-                buf2_data  <= in2_data;
-            end else if (buf2_consumed) begin
-                buf2_valid <= 1'b0;
-            end
-        end
-    end
+    assign in1_ready     = in2_valid&&(!pipe_valid || out_ready);
+    assign in2_ready     = in1_valid&&(!pipe_valid || out_ready);
 
     always_comb begin
         for (int i = 0; i < P_CH; i++) begin
+            logic unsigned [A_BIT-1:0] x1;
+            logic unsigned [A_BIT-1:0] x2;
+            logic unsigned [A_BIT-1:0] sum;
 
-            calc_result[i*A_BIT+:A_BIT] = buf1_data[i*A_BIT+:A_BIT] + buf2_data[i*A_BIT+:A_BIT];
+            x1                          = in1_data[i*A_BIT+:A_BIT];
+            x2                          = in2_data[i*A_BIT+:A_BIT];
+            sum                         = x1 + x2;
+
+            calc_result[i*A_BIT+:A_BIT] = sum;
         end
     end
-
-    wire adder_ready_to_compute = buf1_valid && buf2_valid;
-    wire pipe_push = adder_ready_to_compute && (!out_valid || out_ready);
-    
-    assign buf1_consumed = pipe_push;
-    assign buf2_consumed = pipe_push;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             pipe_valid <= 1'b0;
             pipe_data  <= '0;
         end else begin
-            if (pipe_push) begin
+            if (handshake_in) begin
                 pipe_valid <= 1;
                 pipe_data <= calc_result;
             end else if (handshake_out) begin
